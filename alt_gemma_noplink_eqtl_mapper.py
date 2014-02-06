50230a74a2834bd933429c5595440e9ab20e0374
Darren Cusanovich <darrencusanovich@gmail.com>
2014-02-06 17:00:23 -0600

    Better commenting in eQTL mappers.

--- alt_gemma_noplink_eqtl_mapper.py
+++ alt_gemma_noplink_eqtl_mapper.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-####Import needed modules
 import os
 import sys
 sys.path.append('/mnt/lustre/home/cusanovich/Programs/')
@@ -10,7 +9,6 @@ import random
 import gzip
 import time
 
-####Check that proper arguments are supplied
 #if len(sys.argv) != 3:
 #	print "Usage = python gemma_eqtl_mapper.py chr pc"
 #	sys.exit()
@@ -23,8 +21,6 @@ genodir = '/mnt/lustre/home/cusanovich/500HT/3chip/'
 hmdir = '/mnt/lustre/home/cusanovich/'
 currfiles = genodir + "curr_" + chrm + "_pc" + str(pcs)
 
-####Define handy functions: 'ifier' helps to communicate with the shell from python
-#'matrix_reader' reads in large datasets more quickly and efficiently
 def ifier(commander):
 	ify = subprocess.Popen(commander,shell=True)
 	ify.wait()
@@ -41,12 +37,9 @@ def matrix_reader(matrix_file,sep="\t",dtype='|S20'):
 	rawin.close()
 	return raws
 
-####Added this because Plink seems to crash if multiple jobs are trying to access the same plink file at the same time
+print "Loading expression..."
 time.sleep(random.randint(1,150))
-####Read in map of which columns of expression matrix have which SNPs in cis
 mastercols = matrix_reader('/mnt/lustre/home/cusanovich/500HT/hutt.3chip.mastercols.txt',dtype='|S15')
-####Build dictionaries (like a hash in perl) to (a) keep track of which SNPs go with which genes,
-####(b) keep track of which column of the expression matrix belongs to each gene, and (c) which chromosome each gene is on
 masterdic = {}
 exprcoldic = {}
 chrmdic = {}
@@ -71,7 +64,6 @@ pcdic = {}
 
 #t0 = time.time()
 
-####Loop through each gene to calculate eQTL p-values
 for gene in masterdic.keys():
 #for gene in chr22ers[0:1]:
 #for gene in masterdic.keys()[1:500]:
@@ -81,24 +73,20 @@ for gene in masterdic.keys():
 #		continue
 	print gene
 	#print "Extracting SNPs..."
-	####Pull genotypes for all cis SNPs for the current gene
 	extracter = "plink --noweb --bfile " + genodir + "hutt.3chip --from " + masterdic[gene][0] + " --to " + masterdic[gene][-1] + " --make-bed --out " + currfiles
 	ifier(extracter)
 	#if findivs[0] not in open(currfiles + '.fam','r').readline():
 	#	print "Warning! Genotype findivs do not match phenotype findivs!"
 	#	sys.exit()
 	#print "Prepping GEMMA inputs..."
-	####Creating files to feed into GEMMA
 	famer = ('cut -f1-5 -d" " ' + currfiles + '.fam > ' + currfiles + '.header.fam; cut -f' + str(int(exprcoldic[gene]) + 6) + ' -d" " ' + genodir +
 		'hutt.3chip.fam.update > ' + currfiles + '.tailer.fam; paste -d" " ' + currfiles + '.header.fam ' + currfiles + '.tailer.fam > ' +
 		currfiles + '.fam; rm ' + currfiles + '.header.fam; rm ' + currfiles + '.tailer.fam')
 	ifier(famer)
 	#print "Running GEMMA..."
-	####Calling GEMMA with the current cis SNPs and the current gene
 	gemmer = ('cd ' + genodir + '; ' + hmdir + 'Programs/gemma0.94 -bfile curr_' + chrm + '_pc' + str(pcs) + ' -km 2 -k ' + hmdir + '500HT/addSNP.500ht.txt -c ' + hmdir + '500HT/Exprs/qqnorm.500ht.3chip_order.pc' + str(pcs) + ' -lmm 2 -o curr_' + chrm + '_pc' + str(pcs))
 	ifier(gemmer)
 	currresults = open(genodir + '/output/curr_' + chrm + '_pc' + str(pcs) + '.assoc.txt','r')
-	####Collecting best cis SNP from GEMMA results
 	pmin = 1.1
 	snpmin = ''
 	for line in currresults:
@@ -117,11 +105,9 @@ for gene in masterdic.keys():
 	winnerperms = 0
 	broken = 0
 	findic = {}
-	####Setting up for permutations
 	for perm in range(10000):
 		if broken == 1:
 			continue
-		####Building dictionaries to keep track of which expression and pcs belong to which individual
 		if perm == 0:
 			try:
 				test = findic.keys()[0]
@@ -142,7 +128,6 @@ for gene in masterdic.keys():
 				for z,line in enumerate(pcfile):
 					pcdic[findivs[z]] = line.strip().split()
 				pcfile.close()
-		####Randomize findivs and ask plink to rearrange the genotype files
 		random.shuffle(randfindivs)
 		rearrange = open(currfiles + '.findivs.rearrange.txt','w')
 		for z in range(len(findivs)):
@@ -150,7 +135,6 @@ for gene in masterdic.keys():
 		rearrange.close()
 		plinker = 'plink --noweb --bfile ' + currfiles + ' --update-ids ' + currfiles + '.findivs.rearrange.txt --make-bed --out ' + genodir + 'perm_curr_' + chrm + '_pc' + str(pcs)
 		ifier(plinker)
-		####Put the expression and PC data back in order so that the link between genotypes and phenotypes is broken
 		permfam = open(genodir + 'perm_curr_' + chrm + '_pc' + str(pcs) + '.fam','w')
 		permpc = open(genodir + 'perm_curr_' + chrm + '_pc' + str(pcs) + '.pcs','w')
 		for z in range(len(findivs)):
@@ -159,10 +143,8 @@ for gene in masterdic.keys():
 		permfam.close()
 		permpc.close()
 		print str(winnerperms)
-		####Run GEMMA on permuted data
 		gemmer = ('cd ' + genodir + '; ' + hmdir + 'Programs/gemma0.94 -bfile perm_curr_' + chrm + '_pc' + str(pcs) + ' -km 2 -k ' + hmdir + '500HT/addSNP.500ht.txt -c ' + genodir + 'perm_curr_' + chrm + '_pc' + str(pcs) + '.pcs' + ' -lmm 2 -o perm_curr_' + chrm + '_pc' + str(pcs))
 		ifier(gemmer)
-		####Count if any cis permuted p-value is better than observed cis p-value
 		perms = []
 		permer = open(genodir + 'output/perm_curr_' + chrm + '_pc' + str(pcs) + '.assoc.txt','r')
 		permlow = 1
@@ -178,10 +160,8 @@ for gene in masterdic.keys():
 		if winnerperms == 10:
 			genep = 11/random.uniform(perm+2,perm+3)
 			broken = 1
-	####Calculate permuted p-value
 	if broken == 0:
 		genep = (winnerperms + 1)/float(10001)
-	####Record permuted p-value
 	winnerdic[gene] = [snpmin, pmin, genep]
 	cleanup = 'rm ' + hmdir + '500HT/3chip/*curr_' + chrm + '_pc' + str(pcs) + '*'
 	ifier(cleanup)
@@ -191,14 +171,12 @@ for gene in masterdic.keys():
 #print t1-t0
 
 print "Writing results..."
-####Write out all GEMMA p-values
 aller = open('/mnt/lustre/home/cusanovich/500HT/ByChr/' + chrm + '.PC' + str(pcs) + '.3chip.gemma.eqtls.txt','w')
 for i in range(len(genes)):
 	print >> aller, '{0}\t{1}\t{2:.4g}'.format(genes[i],snps[i],pvals[i])
 
 aller.close()
 
-####Write out best SNP and permuted p-value for each gene
 winners = open('/mnt/lustre/home/cusanovich/500HT/ByChr/' + chrm + '.PC' + str(pcs) + '.3chip.gemma.chosen.txt','w')
 for gene in sorted(winnerdic.keys()):
 	print >> winners, '{0}\t{1[0]}\t{1[1]:.4g}\t{1[2]:.4g}'.format(gene,winnerdic[gene])
--- alt_spearman_eqtl_mapper.py
+++ alt_spearman_eqtl_mapper.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+####Import needed modules
 import os
 import sys
 sys.path.append('/mnt/lustre/home/cusanovich/Programs/')
@@ -12,6 +13,7 @@ import scipy
 from scipy import stats
 #import time
 
+####Check that proper arguments are supplied
 if len(sys.argv) != 3:
 	print "Usage = python spearman_eqtl_mapper.py chr pc"
 	sys.exit()
@@ -20,6 +22,8 @@ chrm = sys.argv[1]
 pcs = sys.argv[2]
 #chrm = 'chr22'
 
+####Define handy functions: 'ifier' helps to communicate with the shell from python
+#'matrix_reader' reads in large datasets more quickly and efficiently
 def ifier(commander):
 	ify = subprocess.Popen(commander,shell=True)
 	ify.wait()
@@ -36,9 +40,13 @@ def matrix_reader(matrix_file,sep="\t",dtype='|S20'):
 	rawin.close()
 	return raws
 
+####Read in expression matrix that has been corrected for relatedness and had some arbitrary number of PCs removed
 print "Loading expression..."
 exprs = matrix_reader('/mnt/lustre/home/cusanovich/500HT/Exprs/qqnorm.500ht.bimbam.PC' + str(pcs) + '.fixed',dtype='|S10')
+####Read in map of which columns of expression matrix have which SNPs in cis
 mastercols = matrix_reader('/mnt/lustre/home/cusanovich/500HT/hutt.3chip.500kb.mastercols.txt',dtype='|S15')
+####Build dictionaries (like a hash in perl) to (a) keep track of which SNPs go with which genes
+####and (b) keep track of which column of the expression matrix belongs to each gene
 masterdic = {}
 exprcoldic = {}
 for i in range(mastercols.shape[0]):
@@ -48,14 +56,17 @@ for i in range(mastercols.shape[0]):
 		masterdic[mastercols[i,0]] = [mastercols[i,1]]
 		exprcoldic[mastercols[i,0]] = mastercols[i,2]
 
+####Read in order of individuals in expression data
 naming = open('/mnt/lustre/home/cusanovich/500HT/findivs.500ht.txt','r')
 exprnames = naming.readlines()
 exprnames = [x.strip().split()[0] for x in exprnames]
 exprgenes = list(set(mastercols[:,0]))
+####Build a dictionary of the actual expression values for each gene
 exprdic = {}
 for gene in exprcoldic.keys():
 	exprdic[gene] = exprs[:,exprcoldic[gene]]
 
+####Build a dictionary to reference the genomic coordinates of each SNP
 print "Loading SNP annotations..."
 snpdic = {}
 snpbed = open('/mnt/lustre/home/cusanovich/500HT/hutt.3chip.hg19.bed','r')
@@ -63,6 +74,7 @@ for line in snpbed:
 	liner = line.strip().split()
 	snpdic[liner[3]] = liner[0:3]
 
+####Read in order of individuals in genotype data
 #genonames = matrix_reader('/mnt/lustre/home/cusanovich/500HT/Imputed1415/imputed_cgi.fam',sep=" ")
 genonames = matrix_reader('/mnt/lustre/home/cusanovich/oldhome_nb/Hutterite_Heritability/gemma/Genotypes/hutt.3chip.hg19remap.fam',sep=" ")
 genonames = list(genonames[:,1])
@@ -73,21 +85,26 @@ except ValueError:
 	print z
 	sys.exit()
 
-#1 - for each gene, collect all SNPs
-#2 - for each SNP calculate p-value in R
-#3 - pick minimum p-value (pick closest if tied)
-#4 - permute genotypes (keep LD structure) x 10000
-#5 - recalculate p-values and pick mins in R
-#6 - after each permutation, check if 10 permutations more extreme -> estimate perm p-value in R
-#7 - calculate p-value -> (1 + sum of perms more extreme)/(1 + No. Permutations)
-#8 - write out all p-values
-#9 - write out gene-level p-values and chosen SNP
+
+####Outline of strategy to follow
+####1 - for each gene, collect all SNPs
+####2 - for each SNP calculate p-value in R
+####3 - pick minimum p-value (pick closest if tied)
+####4 - permute genotypes (keep LD structure) x 10000
+####5 - recalculate p-values and pick mins in R
+####6 - after each permutation, check if 10 permutations more extreme -> estimate perm p-value in R
+####7 - calculate p-value -> (1 + sum of perms more extreme)/(1 + No. Permutations)
+####8 - write out all p-values
+####9 - write out gene-level p-values and chosen SNP
+
+####Function to calculate spearman cor p-value in R
 obsp = robjects.r('function(exprs,genos){\n'
 				 'x = as.numeric(unlist(exprs))\n'
 				 'y = as.numeric(unlist(genos))\n'
 				 'corr = cor.test(x,y,method="spearman",exact=FALSE)$estimate\n'
 				 'pval = cor.test(x,y,method="spearman",exact=FALSE)$p.value\n'
 				 'return(c(corr,pval))}')
+####Function to calculate gene-wise permutation p-value in R
 corp = robjects.r('function(exprs,genos,obsp){\n'
 				  'x = as.numeric(unlist(exprs))\n'
 				  'y = matrix(unlist(genos),length(unlist(genos))/431,431)\n'
@@ -111,16 +128,20 @@ minors = []
 genodic = {}
 winnerdic = {}
 #t0 = time.time()
+####Loop through each gene to calculate eQTL p-values
 for gene in masterdic.keys():
 #for gene in masterdic.keys()[1:500]:
+	####Skip genes not on current chromosome
 	if snpdic[masterdic[gene][0]][0] != chrm:
 		continue
+	####Pull expression data for the gene
 	x = exprdic[gene]
 	snping = [0]*len(masterdic[gene])
 	snpmin = []
 	genemin = []
 	pmin = 1
 	genolist = []
+	####Pull genotypes for the SNPs in cis, if genotypes not already in dictionary: go to geno file and pull in appropriate data
 	for i, snp in enumerate(masterdic[gene]):
 		try:
 			y = genodic[snp]
@@ -143,6 +164,7 @@ for gene in masterdic.keys():
 				genodic[snp] = 'NA'
 				continue
 			genodic[snp] = y
+		####Calculate and record min p-value for each gene
 		currcor = obsp(list(x),y)
 		snping[i] = currcor[1]
 		#print currcor[1]
@@ -154,19 +176,23 @@ for gene in masterdic.keys():
 		if currcor[1] < pmin:
 			snpmin = snp
 			pmin = currcor[1]
+	####Calculate permuted p-value for each gene
 	genep = corp(list(x),genolist,pmin)
+	####Record chosen eQTL for each gene
 	winnerdic[gene] = [snpmin, pmin, genep[0]]
 
 #t1 = time.time()
 #print t1-t0
 
 print "Writing results..."
+####Write out all spearman p-values
 aller = open('/mnt/lustre/home/cusanovich/500HT/ByChr/' + chrm + '.PC' + str(pcs) + '.3chip.500kb.eqtls.txt','w')
 for i in range(len(genes)):
 	print >> aller, '{0}\t{1}\t{2:.3g}\t{3:.4g}'.format(genes[i],snps[i],obs[i],pvals[i])
 
 aller.close()
 
+####Write out best SNP and permuted p-value for each gene
 winners = open('/mnt/lustre/home/cusanovich/500HT/ByChr/' + chrm + '.PC' + str(pcs) + '.3chip.500kb.chosen.txt','w')
 for gene in sorted(winnerdic.keys()):
 	print >> winners, '{0}\t{1[0]}\t{1[1]:.4g}\t{1[2]:.4g}'.format(gene,winnerdic[gene])

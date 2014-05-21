00e5fa62921814e9cbd1e667cf5ebe16014b5f93
Darren Cusanovich <darrencusanovich@gmail.com>
2014-05-21 10:45:55 -0500

    Updates to tissue enrichment significance testing.

--- gemma_noplink_tissue_mapper.py
+++ gemma_noplink_tissue_mapper.py
@@ -11,6 +11,7 @@ import random
 from random import shuffle
 from random import uniform
 import gzip
+from DarrenTools import ifier, matrix_reader
 #import time
 
 chrm = sys.argv[1]
@@ -22,24 +23,6 @@ os.chdir(genodir)
 hmdir = '/mnt/lustre/home/cusanovich/'
 
 currfiles = genodir + "curr_" + chrm + "_pc" + str(pcs) + "_" + correction
-
-# This function just makes it easier to communicate with the shell from Python
-def ifier(commander):
-	ify = subprocess.Popen(commander,shell=True)
-	ify.wait()
-
-# This function just makes python read in big data files all at once more efficiently
-def matrix_reader(matrix_file,sep="\t",dtype='|S20'):
-	linecounter = subprocess.Popen('wc -l ' + matrix_file, shell=True, stdout=subprocess.PIPE)
-	linecount = int(linecounter.communicate()[0].strip().split()[0])
-	columncounter = subprocess.Popen('awk -F"' + sep + '" \'{print NF;exit}\' ' + matrix_file, shell=True, stdout=subprocess.PIPE)
-	columncount = int(columncounter.communicate()[0].strip().split()[0])
-	raws = numpy.zeros((linecount,columncount),dtype=dtype)
-	rawin = open(matrix_file,'r')
-	for i,line in enumerate(rawin):
-		raws[i,:] = line.strip().split()
-	rawin.close()
-	return raws
 
 # This is the bit about running permutations
 def permer(gene):
@@ -73,26 +56,14 @@ def permer(gene):
 
 print "Loading expression..."
 # This built up a list cis eQTLs, we can remove references to it
-mastercols = matrix_reader('/mnt/lustre/home/cusanovich/500HT/hutt' + mapper + '.' + distance + '.mastercols.txt',dtype='|S15')
 
+overlaps = open(['SOME FILE OF THE SNPs OLAPPING DNASE'],'r')
+dnasetable = DarrenTools.matrix_reader(['SAME FILE'])
 masterdic = {}
-exprcoldic = {}
-chrmdic = {}
-for i in range(mastercols.shape[0]):
-	try:
-		masterdic[mastercols[i,0]].append(mastercols[i,1])
-	except KeyError:
-		masterdic[mastercols[i,0]] = [mastercols[i,1]]
-		exprcoldic[mastercols[i,0]] = mastercols[i,2]
-		chrmdic[mastercols[i,0]] = mastercols[i,4]
-
-####Build a dictionary to reference the genomic coordinates of each SNP
-print "Loading SNP annotations..."
-snpdic = {}
-snpbed = open('/mnt/lustre/home/cusanovich/500HT/hutt.imputed.coord.bed','r')
-for line in snpbed:
+for line in overlaps:
 	liner = line.strip().split()
-	snpdic[liner[3]] = liner[0:3]
+	masterdic[liner[3]] = mastercols[0:3]
+	## Also make a dhsdic dictionary of SNPs and the HSs they overlap
 
 #Just copies covariance matrix so parallel processes don't all have to hit the same file over and over
 cover = ('cp ' + hmdir + '500HT/addSNP.500ht.ordered.square.txt ' + currfiles + '.square.txt')
@@ -102,11 +73,6 @@ ifier(cover)
 cover = ('cp ' + hmdir + '[SOME GWAS PHENOTYPE FILE] ' + currfiles + '.pheno')
 ifier(cover)
 
-#chr22ers = []
-#for gene in masterdic.keys():
-#	if chrmdic[gene] == chrm:
-#		chr22ers.append(gene)
-
 #completedgenes = 0
 pvals = []
 genes = []
@@ -114,65 +80,34 @@ snps = []
 winnerdic = {}
 genodic = {}
 randind = range(3,434)
-#Time-savers
-geneappend = genes.append
-snpappend = snps.append
-pvalappend = pvals.append
 #t0 = time.time()
 
 for gene in masterdic.keys():
-	if chrmdic[gene] != chrm:
-		continue
 #	if completedgenes == 5:
 #		break
-	print gene
-	currgenos = []
 	####Pull genotypes for the SNPs in cis, if genotypes not already in dictionary: go to geno file and pull in appropriate data
-	for snp in masterdic[gene]:
-		try:
-			currgenos.append(", ".join(genodic[snp]))
-		except KeyError:
-			#tabixer = pysam.Tabixfile('/mnt/lustre/home/cusanovich/500HT/Imputed1415/ByChr/hutt.imputed.' + chrm + '.txt.gz')
-			#tabixer = pysam.Tabixfile('/mnt/lustre/home/cusanovich/500HT/' + mapper + '/ByChr/hutt.' + mapper + '.' + distance + '.' + chrm + '.txt.gz')
-			tabixer = Tabixfile('/mnt/lustre/home/cusanovich/500HT/Imputed1415/ByChr/hutt' + mapper + '.' + chrm + '.txt.gz')
-			genos = [x.split('\t') for x in tabixer.fetch(chrm,int(snpdic[snp][1]),int(snpdic[snp][2]))][0]
-			tabixer.close()
-			y = [genos[3], 'A', 'G'] + genos[6:len(genos)]
-			genodic[snp] = y
-			currgenos.append(", ".join(genodic[snp]))
 	currbimbam = open(currfiles + '.bimbam','w')
-	print >> currbimbam, "\n".join(currgenos)
+	for snp in masterdic[gene]:
+		tabixer = Tabixfile('/mnt/lustre/home/cusanovich/500HT/Imputed1415/ByChr/hutt' + mapper + '.' + chrm + '.txt.gz')
+		genos = [x.split('\t') for x in tabixer.fetch(chrm,int(snpdic[snp][1]),int(snpdic[snp][2]))][0]
+		tabixer.close()
+		y = [genos[3], 'A', 'G'] + genos[6:len(genos)]
+		print >> currbimbam, ", ".join(y)
 	currbimbam.close()
 	#print "Running GEMMA..."
-	if regressPCs:
-		gemmer = (hmdir + 'Programs/gemma0.94 -g ' + currfiles + '.bimbam -p ' + currfiles + '.pheno -k ' + currfiles + '.square.txt -lmm 4 -maf 0.05 -o curr_' + chrm + '_pc' + str(pcs) + '_' + correction)
-		ifier(gemmer)
-	if not regressPCs:
-		gemmer = (hmdir + 'Programs/gemma0.94 -g ' + currfiles + '.bimbam -p ' + currfiles + '.pheno -k ' + currfiles + '.square.txt -c ' + currfiles + '.pcs.txt -lmm 4 -maf 0.05 -o curr_' + chrm + '_pc' + str(pcs) + '_' + correction)
-		ifier(gemmer)
+	gemmer = (hmdir + 'Programs/gemma0.94 -g ' + currfiles + '.bimbam -p ' + currfiles + '.pheno -k ' + currfiles + '.square.txt -lmm 4 -maf 0.05 -o curr_' + chrm + '_pc' + str(pcs) + '_' + correction)
+	ifier(gemmer)
 	currresults = open(genodir + '/output/curr_' + chrm + '_pc' + str(pcs) + '_' + correction + '.assoc.txt','r')
-	pmin = 1.1
-	snpmin = ''
-	currcount = 0
-	for line in currresults:
-		if 'p_lrt' in line or 'nan' in line:
-			continue
-		liner = line.strip().split()
-		geneappend(gene)
-		snpappend(liner[1])
-		pvalappend(float(liner[12]))
-		currcount = currcount + 1
-		if float(liner[12]) < pmin:
-			pmin = float(liner[12])
-			snpmin = liner[1]
-	if pmin == 1.1:
-		continue
-	#print "Starting up permutations..."
-	if bonferroni:
-		genep = min(1,pmin*currcount)
-	else:
-		genep = permer(gene)
-	winnerdic[gene] = [snpmin, pmin, genep]
+	currresults = matrix_reader(genodir + '/output/curr_' + chrm + '_pc' + str(pcs) + '_' + correction + '.assoc.txt',dtype='f8')
+	currsort = curresults[currresults[:,12].argsort()]
+	currwins = currsort[0:100,]
+	currscores = [0]*len(dhsdic[dhs.keys()[0]])
+	for snp in currwins[:,1]:
+		currscores = currscores + dhsdic[snp]
+	currperms = [0]*len(dhsdic[dhs.keys()[0]])
+	currpermwins = [0]*len(dhsdic[dhs.keys()[0]])
+	curractive = [0]*len(dhsdic[dhs.keys()[0]])
+	tissueps = permer(scores = currscores, circuit = currperms, permwins = currpermwins, actives=curractive)
 	#completedgenes += 1
 
 #t1 = time.time()

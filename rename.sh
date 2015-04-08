08e7a764380855ecb9f20da01a22cbcfb7c28548
Darren Cusanovich <darrencusanovich@gmail.com>
2015-04-08 05:26:58 -0500

    More reorganization.

--- rename.sh
+++
@@ -1,8 +0,0 @@
-DIR="/mnt/lustre/home/cusanovich/500HT/ByChr/"
-for file in `ls ${DIR} | grep 150kbbonferroni | grep chosen`
-do
-#echo $DIR$file
-filenew=`echo $file | sed 's/150kbbonferroni/150kb.bonferroni/g'`
-#echo $filenew
-mv $DIR$file $DIR$filenew
-done

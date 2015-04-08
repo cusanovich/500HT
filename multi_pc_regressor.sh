3e3408e9bb2934fec2f17c9fa555df5b3abea6ff
Darren Cusanovich <darrencusanovich@gmail.com>
2015-04-08 05:16:27 -0500

    Try to get deletions to update.

--- multi_pc_regressor.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in {17..20}
-do
-   nohup python /mnt/lustre/home/cusanovich/500HT/Scripts/combiner.py $i &
-done
--- sherlock_results_eval.R
+++
@@ -1,9 +0,0 @@
-#sherlockdir = "/mnt/lustre/home/cusanovich/500HT/MinalSherlock/"
-sherlockdir = "/mnt/lustre/home/cusanovich/ForMinal/"
-sherlockers = list.files(sherlockdir,pattern=".tidy.results.txt")
-for(i in 1:length(sherlockers)){
-	currsher = read.table(paste0(sherlockdir,sherlockers[i]))
-	adjps = p.adjust(currsher[,3],method="BH")
-	print(sherlockers[i])
-	print(length(which(adjps < 0.2)))
-}
